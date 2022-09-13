ef78b73f88b4fba534243657d06147d1cdfce488
Natália Sapucaia <natisapucaia@gmail.com>
2022-09-13 09:35:50 -0300

    feat: Add a account summary screen

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
-    let dummyViewController = DummyViewController()
     let mainViewController = MainViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
@@ -27,12 +26,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
-        dummyViewController.logoutDelegate = self
 
-        window?.rootViewController = mainViewController
-//        window?.rootViewController = onboardingContainerViewController
+        let vc = mainViewController
+        vc.setStatusBar()
 
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
 
+        window?.rootViewController = vc
 
         return true
     }
@@ -41,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate: LoginViewControllerDelegate {
     func didiLogin() {
         if LocalState.hasOnboarded {
-            setRootViewController(dummyViewController)
+            setRootViewController(mainViewController)
         } else {
             setRootViewController(onboardingContainerViewController)
         }
@@ -51,7 +52,7 @@ extension AppDelegate: LoginViewControllerDelegate {
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
         LocalState.hasOnboarded = true
-        setRootViewController(dummyViewController)
+        setRootViewController(mainViewController)
     }
 }
 
---
+++ Bankey/BankeyUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,36 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankeyUnitTests
+//
+//  Created by Natália Sapucaia on 17/09/22.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class Test: XCTestCase {
+    var formatter: CurrencyFormatter!
+
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466)
+        XCTAssertEqual(result, "$929,466.00")
+    }
+
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0)
+        XCTAssertEqual(result, "$0.00")
+    }
+}
